fe0d3f82598a6267943d54e1ea28a0d635bdc045
Matt Colton <matt.colton@gmail.com>
2017-07-17 10:20:33 -0500

    Modified provider for changes in 0.4.0

--- schem-1.tf
+++ schem-1.tf
@@ -11,23 +11,23 @@ variable cn_hostname {}
 variable computenode_count {}
 variable domain_script_url {}
 
-provider "ibmcloud" {
+provider "ibm" {
   softlayer_username = "${var.softlayer_username}"
   softlayer_api_key = "${var.softlayer_api_key}"
 }
 
-data "ibmcloud_infra_image_template" "base_template" {
+data "ibm_compute_image_template" "base_template" {
   name = "${var.base_template_image}"
 }
-data "ibmcloud_infra_image_template" "compute_template" {
+data "ibm_compute_image_template" "compute_template" {
   name = "${var.compute_node_image}"
 }
 
-resource "ibmcloud_infra_virtual_guest" "domaincontroller" {
+resource "ibm_compute_vm_instance" "domaincontroller" {
   count = "1"
   hostname = "${var.dc_hostname}"
   domain = "${var.domain}"
-  image_id = "${data.ibmcloud_infra_image_template.base_template.id}"
+  image_id = "${data.ibm_compute_image_template.base_template.id}"
   datacenter = "${var.datacenter}"
   cores = 4
   memory = 4096
@@ -39,11 +39,11 @@ resource "ibmcloud_infra_virtual_guest" "domaincontroller" {
   user_metadata = "#ps1_sysnative\nscript: |\n<powershell>\nNew-Item c:\\installs -type directory\ninvoke-webrequest '${var.domain_script_url}' -outfile 'c:\\installs\\create-domain-controller.ps1'\nc:\\installs\\create-domain-controller.ps1 -domain ${var.domain} -username ${var.domain_username} -password ${var.domain_password} -step 1\n</powershell>"
 }
 
-resource "ibmcloud_infra_virtual_guest" "computenodes" {
+resource "ibm_compute_vm_instance" "computenodes" {
   count = "${var.computenode_count}"
   hostname = "${var.cn_hostname}${count.index}"
   domain = "${var.domain}"
-  image_id = "${data.ibmcloud_infra_image_template.compute_template.id}"
+  image_id = "${data.ibm_compute_image_template.compute_template.id}"
   datacenter = "${var.datacenter}"
   cores = 2
   memory = 2048
@@ -64,7 +64,7 @@ resource "ibmcloud_infra_virtual_guest" "computenodes" {
     $secure_string_pwd = ConvertTo-SecureString "${var.domain_password}" -AsPlainText -Force
     $cred = New-Object System.Management.Automation.PSCredential ("${var.domain_username}", $secure_string_pwd)
     $private_nic = Get-NetAdapter -Name "Ethernet 2"
-    $private_nic | Set-DnsClientServerAddress -ServerAddresses ("${ibmcloud_infra_virtual_guest.domaincontroller.ipv4_address_private}")
+    $private_nic | Set-DnsClientServerAddress -ServerAddresses ("${ibm_compute_vm_instance.domaincontroller.ipv4_address_private}")
     Sleep -Seconds 5
     Add-Computer -DomainName "${var.domain}" -Credential $cred
     Sleep -Seconds 5
