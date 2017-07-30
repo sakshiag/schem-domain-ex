d90a34b56bc425527009277c5288302f40d4e902
Matt Colton <matt.colton@gmail.com>
2017-07-30 11:57:17 -0500

    Fxed script

--- schem-1.tf
+++ schem-1.tf
@@ -50,7 +50,7 @@ resource "ibm_compute_vm_instance" "domaincontroller" {
     $uri = "https://openwhisk.ng.bluemix.net/api/v1/web/mcolton%40us.ibm.com_dev/default/buildHPC.json"
     invoke-webrequest -uri $uri -Method Post -body $body
     '@
-    $s | out-file "c:\installs\create-computenodes.ps1"
+    $s | out-file 'c:\installs\create-computenodes.ps1'
 
     invoke-webrequest '${var.domaincontroller_script_url}' -outfile 'c:\installs\create-domain-controller.ps1'
     c:\installs\create-domain-controller.ps1 -domain ${var.domain} -username ${var.domain_username} -password ${var.domain_password} -step 1
